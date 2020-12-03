c4181cdf72281f830c43940f2ee9a111bb449980
Mohit Ahuja <mahuja3@illinois.edu>
2020-12-03 14:33:17 -0600

    shifted individual edgelists in airport objects to complete edgelist in graph class

--- airport.h
+++ airport.h
@@ -9,7 +9,7 @@ class Airport{
         Airport(double longitude, double latitude,std::string airportName,unsigned width, unsigned height); //constructs airport based on inputted longitude and latitude, needs width and height of Mercator png to calculate x and y coordinates
         void increaseSize();
     private:
-        std::vector<Routes> routes; //edgelist of outgoing routes from this airport
+        //std::vector<Routes> routes; //edgelist of outgoing routes from this airport
         int x; //x coordinate for mapping onto PNG
         int y; //y coordinate for mapping onto PNG
         double lon; //longitude
--- graph.h
+++ graph.h
@@ -20,6 +20,7 @@ class Graph {
     private:
         //edge holds name of airport and name of destination airport, graph implementations and traversals, adjacency list
         std::unordered_map<int, Airport> airportMap; //hash map that maps airport ID to airport object
+        std::unordered_map<int,std::vector<Routes>> routeList; //edgelist holding all the routes, key is airport ID and return value is vector of outgoing routes
         std::unordered_map<std::string, std::vector<int>> cities; //hash map that maps city name to vector of airport id's within that city
         cs225::PNG map; //will hold mercator map
 };
