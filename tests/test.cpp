18a35489cc0b1ab924f4c3be68e5f8bda9f68835
Mohit Ahuja <mahuja3@illinois.edu>
2020-12-09 20:24:03 -0600

    added test files

---
+++ tests/test.cpp
@@ -0,0 +1,15 @@
+#include "../cs225/catch/catch.hpp"
+
+#include "../graph.h"
+#include "../cs225/PNG.h"
+
+// #include "../edge.h"
+// #include "../NetworkFlow.h"
+
+#include <iostream>
+
+TEST_CASE("Graph a - 10 -> b  has flow of 10.", "[valgrind][weight=1]") {
+  //Graph g = Graph(true,true);
+
+  
+}
